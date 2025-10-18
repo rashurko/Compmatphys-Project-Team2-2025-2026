8c6b0143c5496da190b338cb53b0b534bfb11a66
vsc21136 vscuser <vsc21136@gligar10.gastly.os>
2025-10-18 17:28:57 +0200

    Convergence tests

---
+++ convergence_testing/k_testing.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#
+#
+#PBS -N jobTiFe
+#PBS -o output_TiFe.file
+#PBS -e errorTiFe.file
+#PBS -l walltime=0:30:00
+#PBS -l nodes=1:ppn=8
+#PBS -l mem=5gb
+#
+# change to directory you were working when submitting job
+cd $PBS_O_WORKDIR
+#load QE
+module load QuantumESPRESSO
+#run QE command
+mpirun -np $PBS_NP pw.x -input TiFe.in > TiFe_k_15_wfc_250_f_20.out
+
