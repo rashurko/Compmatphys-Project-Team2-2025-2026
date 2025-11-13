e733089c1585e2036d99748a1e5fdc7666de7822
vsc49058 vscuser <vsc49058@gligar10.gastly.os>
2025-11-13 13:25:12 +0100

    Geometry optimization for TiNi_x

---
+++ geometry_optimization_step_3/TiNi2/submit_job.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#
+#
+#PBS -N jobTiFe
+#PBS -o output_HfFe2.file
+#PBS -e errorTiFe.file
+#PBS -l walltime=24:00:00
+#PBS -l nodes=1:ppn=16
+#PBS -l mem=22gb
+#
+# change to directory you were working when submitting job
+cd $PBS_O_WORKDIR
+#load QE
+module load QuantumESPRESSO
+#run QE command
+mpirun -np $PBS_NP pw.x -input TiNi2_vc_relax_mag.in > TiNi2_vc_relax_mag.out
+
