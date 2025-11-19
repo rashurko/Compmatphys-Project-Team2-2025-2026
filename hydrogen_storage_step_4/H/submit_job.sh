c210869b13f091de673da151049386d6d29a1abf
vsc49058 vscuser <vsc49058@gligar09.gastly.os>
2025-11-20 00:25:57 +0100

    fixed vc-relax calculations for H and TiNiH_05_05_00

--- hydrogen_storage_step_4/H/submit_job.sh
+++ hydrogen_storage_step_4/H/submit_job.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 #
 #
-#PBS -N jobTiFe
-#PBS -o output_TiFe.file
-#PBS -e errorTiFe.file
-#PBS -l walltime=3:00:00
+#PBS -N jobH
+#PBS -o output_H.file
+#PBS -e errorH.file
+#PBS -l walltime=6:00:00
 #PBS -l nodes=1:ppn=16
-#PBS -l mem=5gb
+#PBS -l mem=10gb
 #
 # change to directory you were working when submitting job
 cd $PBS_O_WORKDIR
 #load QE
 module load QuantumESPRESSO
 #run QE command
-mpirun -np $PBS_NP pw.x -input H.in > H_vc_relax.out
+mpirun -np $PBS_NP pw.x -input H.in > H_vc_relax_mag_newpot.out
